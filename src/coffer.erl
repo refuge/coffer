73ed82e33116b71aa9e38f121e2d3a1dc098606e
benoitc <bchesneau@gmail.com>
2013-02-17 09:22:45 +0100

    add possibility to start coffer directly from the shell.
    allows to start coffer app using `coffer:start/0`. While I'm here make sure we
    start all coffer deps.

--- src/coffer.erl
+++ src/coffer.erl
@@ -7,9 +7,23 @@
 
 -include_lib("coffer/includes/coffer.hrl").
 
+-export([start/0, stop/0]).
 -export([list_resources/0, add_resource/3, remove_resource/1]).
 -export([open/1, open/2, close/1]).
 -export([put/3, get/2, get/3, delete/2, all/1, foldl/3, foldl/4, foreach/2]).
+
+
+%% @doc Start the coffer application. Useful when testing using the shell.
+start() ->
+    coffer_deps:ensure(),
+    application:load(coffer),
+    coffer_app:ensure_deps_started(),
+    application:start(coffer).
+
+%% @doc Start the coffer application. Useful when testing using the shell.
+stop() ->
+    application:stop(coffer).
+
 
 list_resources() ->
     coffer_resource:list().
--- src/coffer_app.erl
+++ src/coffer_app.erl
@@ -4,22 +4,32 @@
 
 %% Application callbacks
 -export([start/2, stop/1]).
+-export([ensure_deps_started/0, ensure_started/1]).
 
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    Dispatch = [
-        {'_', [
-            {[<<"blob">>, '...'], coffer_main_handler, []}
-        ]}
-    ],
-    {ok, _} = cowboy:start_http(http, 100, [{port, 8080}], [
-        {dispatch, Dispatch}
-    ]),
-
+    coffer_deps:ensure(),
+    ensure_deps_started(),
     coffer_sup:start_link().
 
 stop(_State) ->
     ok.
+
+
+ensure_deps_started() ->
+    {ok, Deps} = application:get_key(coffer, applications),
+    true = lists:all(fun ensure_started/1, Deps).
+
+ensure_started(App) ->
+    case application:start(App) of
+        ok ->
+            true;
+        {error, {already_started, App}} ->
+            true;
+        Else ->
+            error_logger:error_msg("Couldn't start ~p: ~p", [App, Else]),
+            Else
+    end.
