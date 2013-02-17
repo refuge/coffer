3f2171256d32399980a6e254411d9ee9e9593906
benoitc <bchesneau@gmail.com>
2013-02-17 09:04:25 +0100

    expose resource methods to main module

--- src/coffer.erl
+++ src/coffer.erl
@@ -7,12 +7,18 @@
 
 -include_lib("coffer/includes/coffer.hrl").
 
--export([list_resources/0]).
+-export([list_resources/0, add_resource/3, remove_resource/1]).
 -export([open/1, open/2, close/1]).
 -export([put/3, get/2, get/3, delete/2, all/1, foldl/3, foldl/4, foreach/2]).
 
 list_resources() ->
     coffer_resource:list().
+
+add_resource(Name, Backend, Config) ->
+    coffer_resource:add(Name, Backend, Config).
+
+remove_resource(Name) ->
+    coffer_resource:remove(Name).
 
 open(ResourceName) ->
     open(ResourceName, []).
